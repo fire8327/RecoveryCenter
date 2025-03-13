d1d366e02b1cf1101f86e23730553fc3e9cfc074
fire8327 <fire83274@gmail.com>
2025-03-13 22:41:58 +0300

    create index page

---
+++ components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="grid-container w-full py-4">
+        <div class="flex items-center justify-between">
+            <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+                <img src="/images/header/logo.png" alt="">
+            </NuxtLink>
+            <div class="flex items-center gap-6">
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">Главная</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">Каталог продукции</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">О компании</NuxtLink>
+                <div class="flex items-center gap-2">
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+                        <Icon class="text-3xl text-[#442fe0]" name="material-symbols:shopping-cart-rounded"/>
+                    </NuxtLink>
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+                        <Icon class="text-3xl text-[#442fe0]" name="material-symbols:person"/>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
