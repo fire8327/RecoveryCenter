f1784aa811093f2a286b4f0a6c459c4c32823bc6
fire8327 <fire83274@gmail.com>
2025-03-13 22:45:18 +0300

    upd index page

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,19 @@
 <template>
     <header class="grid-container w-full py-4">
         <div class="flex items-center justify-between">
-            <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+            <NuxtLink to="/" class="transition-all duration-500 hover:opacity-80">
                 <img src="/images/header/logo.png" alt="">
             </NuxtLink>
             <div class="flex items-center gap-6">
-                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">Главная</NuxtLink>
-                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">Каталог продукции</NuxtLink>
-                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-[#442fe0] after:transition-all after:duration-500 hover:after:w-full">О компании</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">Главная</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">Каталог продукции</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">О компании</NuxtLink>
                 <div class="flex items-center gap-2">
-                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
-                        <Icon class="text-3xl text-[#442fe0]" name="material-symbols:shopping-cart-rounded"/>
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-80">
+                        <Icon class="text-3xl text-blue-500" name="material-symbols:shopping-cart-rounded"/>
                     </NuxtLink>
-                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
-                        <Icon class="text-3xl text-[#442fe0]" name="material-symbols:person"/>
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-80">
+                        <Icon class="text-3xl text-blue-500" name="material-symbols:person"/>
                     </NuxtLink>
                 </div>
             </div>
